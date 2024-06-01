fa14ed81ad6b6dc6e51fa429c65924e1d37a4dc2
Vivek <vivekanandareddy7677@gmail.com>
2024-06-01 00:05:22 -0500

    Setting up VPC

--- modules/app/main.tf
+++ modules/app/main.tf
@@ -1,6 +1,7 @@
-#resource "aws_security_group" "main" {
-#
-#}
+resource "aws_security_group" "main" {
+  name   = "${var.component}-${var.env}-sg"
+  vpc_id = var.vpc_id
+}
 
 resource "aws_instance" "instance" {
   ami                    = data.aws_ami.ami.image_id
@@ -35,7 +36,6 @@ resource "null_resource" "ansible" {
       "sudo pip3.11 install ansible hvac",
       "ansible-pull -i localhost, -U https://github.com/DevOps-iteration1/expense-ansible get-secrets.yml -e env=${var.env} -e role_name=${var.component} -e vault_token=${var.vault_token}",
       "ansible-pull -i localhost, -U https://github.com/DevOps-iteration1/expense-ansible expense.yml -e env=${var.env} -e role_name=${var.component} -e @secrets.json -e @app.json",
-      "rm -f ~/secrets.json ~/app.json"
     ]
   }
 
